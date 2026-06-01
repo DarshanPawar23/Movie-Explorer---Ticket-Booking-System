356b9a8009c5ebfa49a4000416afcb3a82749cd7
DarshanPawar23 <darshansp0230@gmail.com>
2026-06-02 00:40:05 +0530

    Movie Api Routes Implementd

--- Backend/Routes/movieRoutes.js
+++ Backend/Routes/movieRoutes.js
@@ -1,18 +1,44 @@
 import express from "express";
 import tmdb from "../Services/tmdbService.js";
 
-const router = express.Router();
+const MovieRouter = express.Router();
 
-router.get("/trending",async(req,res)=>{
+MovieRouter.get("/trending",async(req,res)=>{
     try{
       const response = await tmdb.get("/trending/movie/week");
       res.json(response.data);
     }
     catch(error){
-     res.status(500).json({
+     return res.status(500).json({
       message: "Failed to fetch movies"
     });
     }
 });
 
-export default router;
+MovieRouter.get("/search", async (req, res) => {
+  try {
+    const { query } = query;
+    const response = await tmdb.get(`/search/movie?query=${query}`);
+    return res.status(200).json(response.data);
+  }
+  catch (error) {
+    return res.status(500).json({
+      message: "Search Failed"
+    });
+  }
+});
+
+MovieRouter.get("/movie/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await tmdb.get(`/movie/${id}`);
+    res.status(200).json(response.data);
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Movie Not Found"
+    });
+  }
+});
+export default MovieRouter;
+
